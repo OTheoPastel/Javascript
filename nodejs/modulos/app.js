b712418aa73c7daa28fb701a73d1905a3eac4f94
Theo Freitas <telmapaes1@gmail.com>
2025-08-27 09:25:37 -0300

    versaocalculadoraidade

--- nodejs/modulos/app.js
+++ nodejs/modulos/app.js
@@ -1,4 +1,7 @@
-const saudacao = require('./meuModulo'); // Importando o módulo
+const modulo = require('./meuModulo'); // Importando o módulo
+const calculadora_idade = require('./calculadoraidade'); // Importando o módulo
 
-const mensagem = saudacao('Joédio'); // Executando a função
+const mensagem = modulo.saudacao('Theo'); // Executando a função
+const idadeem2025 = calculadora_idade.idade('Theo' , 2008); // Executando a função
 console.log(mensagem);
+console.log(idadeem2025)
