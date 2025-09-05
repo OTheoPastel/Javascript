5856b45118dd99196bbc51f60c1397919da5e014
Theo Freitas <telmapaes1@gmail.com>
2025-09-05 09:26:51 -0300

    Versão http

--- nodejs/modulos/http/app.js
+++ nodejs/modulos/http/app.js
@@ -1,10 +1,11 @@
 //importando o modelo http
 const http = require('http');
 //criando um sevidor que responde com "Hello World" para todas as requisicoes
-http.createServer((req, res) => {
+http.createServer((req, res)=> {
     //definindo o cabecalho da resposta com status 200 (ok) e tipo de conteudo text/plain
-    req.writeHead(200, {'Content-Type' : 'text/plain'});
+    res.writeHead(200, {'Content-Type' : 'text/plain'});
     //Enviando a resposta "Hello World!"
-    res.end('Hello World!');
+    res.end('Ola mundo!');
     //adicionando um log no console para cada requisicao recebida
 }).listen(8080);
+console.log('Servidor rodando em http://localhost:8080/');
